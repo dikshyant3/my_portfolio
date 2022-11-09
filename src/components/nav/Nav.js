9136c8ae8d3ef5d3924c3cb5001e890b8eaf24e4
Dikshyant <dhunganadikshyant12@gmail.com>
2022-11-09 11:13:44 +0545

    nav-bar created

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Nav.css";
 import { AiOutlineHome } from "react-icons/ai";
 import { AiOutlineUser } from "react-icons/ai";
@@ -6,20 +6,37 @@ import { BiBook } from "react-icons/bi";
 import { BiMessageSquareDetail } from "react-icons/bi";
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <>
       <nav>
-        <a href="/">
+        <a
+          href="#"
+          onClick={() => setActiveNav("#")}
+          className={activeNav === "#" ? "active" : ""}
+        >
           <AiOutlineHome />
         </a>
-        <a href="#about">
+        <a
+          href="#about"
+          onClick={() => setActiveNav("#about")}
+          className={activeNav === "#about" ? "active" : ""}
+        >
           <AiOutlineUser />
         </a>
-        <a href="#experience">
+        <a
+          href="#experience"
+          onClick={() => setActiveNav("#experience")}
+          className={activeNav === "#experience" ? "active" : ""}
+        >
           <BiBook />
         </a>
 
-        <a href="#contact">
+        <a
+          href="#contact"
+          onClick={() => setActiveNav("#contact")}
+          className={activeNav === "#contact" ? "active" : ""}
+        >
           <BiMessageSquareDetail />
         </a>
       </nav>
